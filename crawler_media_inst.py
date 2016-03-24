968c26fabb2d4f4f7922f2eeb1ddc43fa142e093
kugusha <kugush7@gmail.com>
2016-03-24 12:21:44 +0400

    Update crawler_media_inst.py

--- crawler_media_inst.py
+++ crawler_media_inst.py
@@ -1,27 +1,24 @@
 from instagram.client import InstagramAPI
 from instagram.bind import InstagramAPIError
-import json
 from pymongo import MongoClient
 
 client = MongoClient()
-usersDB = client["UsersInstagram"]["public"]
-mediaDB = client["UsersInstagram"]["media"]
+usersDB = client["VK_users"]["users"]
+mediaDB = client["Media"]["vk_media_new"]
 
+access_token = "2206388144.9a41b0c.e3a1a62d62a040abaedc1dd89952e7b6"
+client_secret = "f21605b4441040a4b4de3f42da5cb41e"
 
 # connect to Instagram API
 api = InstagramAPI(access_token = access_token, client_secret = client_secret)
 
-for user in usersDB:
-        try:
-                info = api.user(user_id = user.id)
-                #recent_media 
-                    if not media.find_one({"id" : user.id}):
-                            media.insert(#hz smth like info.media_id)
-                            print "MEDIA ADDED"
-                    else:
-                            print "ALREADY IN"
-        except InstagramAPIError as e:
-            print e
+for user in usersDB.find():
+    try:
+        if not mediaDB.find_one({"id" : user['uid']}):
+            media_data, next_ = api.user_recent_media(user_id = user['uid'])
+            for media in media_data:
+                print media.__dict__["id"]
+                mediaDB.insert({'caption' : str(media.__dict__)})
 
-        print ''
-# check counters and set to Mongo (50)
+    except InstagramAPIError as e:
+        print e
